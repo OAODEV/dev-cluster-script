afd87f4e99abf14cc96c7f6b0a14cf56b7945aa0
Luz M. Costa <luzmcosta@gmail.com>
2018-02-20 23:02:45 -0500

    Config cluster scope + resources

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var container = google.container('v1');
 // OAO's GCP project and [current customary] zone
 var project = 'lexical-cider-93918';
 var zone = 'us-central1-a';
+var default_name = 'playground';
 
 exports.getCluster = function(req, res) { authorize(function(authClient) {
   // Get cluster name from request body
-  var cluster_name = req.body.name  || 'dev-playground';
+  var cluster_name = req.body.name  || default_name;
   var request = {
     projectId: project,  
     zone: zone,
@@ -27,17 +28,38 @@ exports.getCluster = function(req, res) { authorize(function(authClient) {
 
 exports.createCluster = function(req, res) { authorize(function(authClient) {
   // Get cluster name from request body
-  var cluster_name = req.body.name || 'dev-playground';
+  var cluster_name = req.body.name || default_name;
   var request = {
     projectId: project,  
     zone: zone,
     resource: {
-      "cluster": {
-          "name": cluster_name,
-          "description": "Shared Dev cluster for trial/integration deployment.",
-          "initialNodeCount": 3
-      }
-    },
+          "cluster": {
+              "name": cluster_name,
+              "resourceLabels": {
+                "env": "development"
+              },
+              "enableKubernetesAlpha": true,
+              "description": "Shared development cluster for trial/integration deployment.",
+              "initialNodeCount": 3,
+              "nodeConfig": {
+                "diskSizeGb": 100,
+                "imageType": "COS",
+                "machineType": "n1-standard-1",
+                "oauthScopes": [
+                    "https://www.googleapis.com/auth/compute",
+                    "https://www.googleapis.com/auth/devstorage.read_only",
+                    "https://www.googleapis.com/auth/logging.write",
+                    "https://www.googleapis.com/auth/monitoring",
+                    "https://www.googleapis.com/auth/service.management.readonly",
+                    "https://www.googleapis.com/auth/servicecontrol",
+                    "https://www.googleapis.com/auth/trace.append",
+                    "https://www.googleapis.com/auth/cloud-platform",
+                    "https://www.googleapis.com/auth/cloud_debugger",
+                ],
+                "serviceAccount": "default"
+              },
+          }
+      },
     auth: authClient,
   };
 
@@ -55,7 +77,7 @@ exports.createCluster = function(req, res) { authorize(function(authClient) {
 
 exports.deleteCluster = function(req, res) { authorize(function(authClient) {
   // Get cluster name from request body
-  var cluster_name = req.body.name  || 'dev-playground';
+  var cluster_name = req.body.name  || default_name;
   var request = {
     projectId: project,  
     zone: zone,
@@ -82,7 +104,9 @@ function authorize(callback) {
       return;
     }
     if (authClient.createScopedRequired && authClient.createScopedRequired()) {
-      var scopes = ['https://www.googleapis.com/auth/cloud-platform'];
+      var scopes = ['https://www.googleapis.com/auth/cloud-platform',
+                    'https://www.googleapis.com/auth/cloud-debugger',
+                   ];
       authClient = authClient.createScoped(scopes);
     }
     callback(authClient);
